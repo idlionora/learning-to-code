537c80bc3cedee49915fcf2d4f51f1037d9480ba
Nurul Azizah <idlionora@gmail.com>
2023-02-21 10:41:12 +0700

    feat(toDoList): add btn-edit-cancel and btn-edit-confirm, add editTodo and deleteTodo functions

--- frontend-assignments/toDoList.js
+++ frontend-assignments/toDoList.js
@@ -1,13 +1,19 @@
-const todoList = ["Belajar HTML", "Belajar JavaScript"];
+const todoList = ['Belajar HTML', 'Belajar JavaScript'];
 
-const todoInput = document.getElementById("todolist-input");
-const addBtn = document.getElementById('btn-addtodo');
-const todoListContainer = document.getElementById("todo-entry-container");
+const todoInput = document.getElementById('todolist-input');
+const addTodoBtn = document.getElementById('btn-addtodo');
+const cancelEditBtn = document.getElementById('btn-edit-cancel');
+const confirmEditBtn = document.getElementById('btn-edit-confirm');
+const todoListContainer = document.getElementById('todo-entry-container');
+let selectedTodoIndex = null;
+
+console.log(cancelEditBtn);
+console.log(confirmEditBtn);
 
 function showTodoList() {
-    let todoEntries = "";
-    todoList.forEach((todo, index) => {
-        todoEntries += `
+	let todoEntries = '';
+	todoList.forEach((todo, index) => {
+		todoEntries += `
         <li class="todo-entry">
             <p class="todo-text">${todo}</p>
             <div class="todo-slide">
@@ -15,26 +21,56 @@ function showTodoList() {
                 <button class="btn-deletetodo" aria-label="delete" onclick="deleteTodo(${index})"><i class="fas fa-trash" aria-hidden="true"></i></button>
             </div>
         </li>
-        `
-    });
-    todoListContainer.innerHTML = todoEntries;
-}
+        `;
+	});
+	todoListContainer.innerHTML = todoEntries;
+};
 
 function addTodo() {
-    todoList.push(todoInput.value);
-    todoInput.value = "";
-    addBtn.classList.remove("active");
-    showTodoList();
-}
+	todoList.push(todoInput.value);
+	todoInput.value = '';
+	addTodoBtn.classList.remove('active');
+	showTodoList();
+};
 
-todoInput.addEventListener("keyup", e => {
-    let todoValue = e.target.value;
-    if (todoValue.trim().length != 0) {
-		addBtn.classList.add('active');
+function editTodo(index) {
+    cancelEditBtn.classList.remove("hide");
+    confirmEditBtn.classList.remove("hide");
+    addTodoBtn.classList.add("hide");
+    todoInput.value = todoList[index];
+    selectedTodoIndex = index;
+};
+
+function cancelEdit() {
+    cancelEditBtn.classList.add("hide");
+    confirmEditBtn.classList.add("hide");
+    addTodoBtn.classList.remove("hide");
+    todoInput.value = "";
+    selectedTodoIndex = null;
+};
+
+function confirmEdit() {
+    todoList[selectedTodoIndex] = todoInput.value;
+    cancelEdit();
+    showTodoList();
+};
+
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    showTodoList()
+};
+
+todoInput.addEventListener('keyup', (e) => {
+	let todoValue = e.target.value;
+	if (todoValue.trim().length != 0) {
+		addTodoBtn.classList.add('active');
 	} else {
-		addBtn.classList.remove('active');
+		addTodoBtn.classList.remove('active');
 	}
 });
 
-addBtn.addEventListener("click", addTodo)
+addTodoBtn.addEventListener('click', addTodo);
+cancelEditBtn.addEventListener('click', cancelEdit);
+confirmEditBtn.addEventListener("click", confirmEdit);
+
 showTodoList();
